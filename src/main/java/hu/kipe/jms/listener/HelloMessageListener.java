e08ec7493510ef4d46c25f363615b9db81a8e8df
Kis Peter <kis.peter@protonmail.com>
2021-05-27 11:44:54 +0200

    Listener added

---
+++ src/main/java/hu/kipe/jms/listener/HelloMessageListener.java
@@ -0,0 +1,22 @@
+package hu.kipe.jms.listener;
+
+import hu.kipe.jms.config.JmsConfig;
+import hu.kipe.jms.model.HelloWorldMessage;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloMessageListener {
+
+    @JmsListener(destination = JmsConfig.MY_QUEUE)
+    public void listen(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers, Message message) {
+
+        System.out.println("I got a message!");
+        System.out.println(helloWorldMessage);
+    }
+}
