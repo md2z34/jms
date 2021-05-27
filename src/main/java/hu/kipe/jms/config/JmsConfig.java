163cfd38467998ac09894f5299bf5c89ebf90eee
Kis Peter <kis.peter@protonmail.com>
2021-05-27 11:09:48 +0200

    Sender added

--- src/main/java/hu/kipe/jms/config/JmsConfig.java
+++ src/main/java/hu/kipe/jms/config/JmsConfig.java
@@ -7,6 +7,9 @@ import org.springframework.jms.support.converter.MessageType;
 
 @Configuration
 public class JmsConfig {
+
+    public static final String MY_QUEUE = "hello-queue";
+
     public MessageConverter messageConverter() {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
